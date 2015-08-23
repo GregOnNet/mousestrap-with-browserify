6a0149c317fe741502a9f507436ea52fb713848f
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-08-23 19:16:51 +0200

    Adds build script

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp       = require('gulp');
+var concat     = require('gulp-concat');
+var ts         = require('gulp-typescript');
+var browserify = require('gulp-browserify');
+
+var tsConfig = ts.createProject('tsconfig.json');
+
+gulp.task('build', ['copy-index',
+                    'build-ts',
+                    'build-mousetrap']);
+
+gulp.task('build-ts', function() {
+    var tsResult = tsConfig.src()
+                           .pipe(ts(tsConfig));
+
+    return tsResult.js
+                   .pipe(browserify())
+                   .pipe(gulp.dest('./.build/'));
+});
+
+
+gulp.task('build-mousetrap', function() {
+    var tsResult = tsConfig.src()
+                           .pipe(ts(tsConfig));
+
+    return gulp.src(['./node_modules/mousetrap/mousetrap.js'])
+               .pipe(browserify())
+               .pipe(gulp.dest('./.build/'));
+});
+
+gulp.task('copy-index', function() {
+  return gulp
+           .src('./index.html')
+           .pipe(gulp.dest('./.build/'));
+
+});
